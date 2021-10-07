bb9d88d9ee2faaacfcde9f4cf339dd8909b1b3fc
Moses Boriowo <boriowomoses2@gmail.com>
2021-10-07 18:48:27 +0100

    my commit

--- Terraform for AWS Cloud II/AWS-IAM-Roles/S3bucket.tf
+++ Terraform for AWS Cloud II/AWS-IAM-Roles/S3bucket.tf
@@ -1,11 +1,11 @@
 #Create AWS S3 Bucket
 
-resource "aws_s3_bucket" "levelup-s3bucket" {
+resource "aws_s3_bucket" "levelup-mys3bucket" {
   bucket = "levelup-bucket-141"
   acl    = "private"
 
   tags = {
-    Name = "levelup-bucket-141"
+    Name = "levelup-mybucket-141"
   }
 }
 
